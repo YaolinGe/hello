fccafa9e1147101a71a09a9e9aa1b5ae11067c4e
YaolinGe <geyaolin@gmail.com>
2025-07-28 21:48:25 +0200

    udpate

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '''
-        <html>
-            <head>
-                <title>Welcome</title>
-            </head>
-            <body>
-                <h1>Welcome to the Index Page!</h1>
-                <p>This is a simple Flask application.</p>
-                <ul>
-                    <li><a href="https://flask.palletsprojects.com/">Flask Documentation</a></li>
-                    <li><a href="https://github.com/pallets/flask">Flask on GitHub</a></li>
-                </ul>
-            </body>
-        </html>
-    '''
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+   app.run()
